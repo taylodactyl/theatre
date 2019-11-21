525c62ad404cfd47c669707d0e9211ced4133c31
Taylor Ross <tayloross@gmail.com>
2019-11-20 21:39:40 -0600

    Add initial model serializers

---
+++ challenge/theatre/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Room, Movie, Screening, Ticket
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ['id', 'capacity']
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'length']
+
+
+class ScreeningSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Screening
+        fields = ['id', 'room', 'movie', 'time']
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = ['id', 'showing']
+
