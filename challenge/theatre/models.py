66bafecb8f85cfcf85a0a0e3af8b2972aaeb6cd9
Taylor Ross <tayloross@gmail.com>
2019-11-20 21:39:15 -0600

    Add str methods for models

--- challenge/theatre/models.py
+++ challenge/theatre/models.py
@@ -5,10 +5,16 @@ from datetime import timedelta
 class Room(models.Model):
     capacity = models.IntegerField(default=100)
 
+    def __str__(self):
+        return "{} - Seats {}".format(self.id, self.capacity)
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=200)
     length = models.DurationField(default=timedelta(minutes=90))
+
+    def __str__(self):
+        return "{} - {}".format(self.title, self.length)
 
 
 class Screening(models.Model):
@@ -16,7 +22,13 @@ class Screening(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     time = models.TimeField()
 
+    def __str__(self):
+        return "{} - {} @ {}".format(self.room, self.movie, self.time)
+
 
 class Ticket(models.Model):
     showing = models.ForeignKey(Screening, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{}".format(self.showing)
 
