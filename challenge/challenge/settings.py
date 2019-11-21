9085f02b7a6470f6ab6fd754549dfdb8bceabf49
Taylor Ross <tayloross@gmail.com>
2019-11-20 21:07:51 -0600

    Add initial set of models and their initial migration

--- challenge/challenge/settings.py
+++ challenge/challenge/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'theatre.apps.TheatreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- challenge/theatre/models.py
+++ challenge/theatre/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from datetime import timedelta
 
-# Create your models here.
+
+class Room(models.Model):
+    capacity = models.IntegerField(default=100)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    length = models.DurationField(default=timedelta(minutes=90))
+
+
+class Screening(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    time = models.TimeField()
+
+
+class Ticket(models.Model):
+    showing = models.ForeignKey(Screening, on_delete=models.CASCADE)
+
