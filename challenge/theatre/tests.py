21992c8c5adeaf31c9d1d2c7e6cac3385824eb5f
Taylor Ross <tayloross@gmail.com>
2019-11-21 19:25:52 -0600

    Add a quick test for negative capacity rooms

--- challenge/theatre/tests.py
+++ challenge/theatre/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.db.utils import IntegrityError
+from .models import Room
 
-# Create your tests here.
+
+class RoomModelTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_no_negative_capacity(self):
+        with self.assertRaises(IntegrityError):
+            Room(capacity=-1).save()
